5a3fbb8e4b2479bdc4df7a6c4a3574894e9397bc
bsinner <bbllaakkeederp@gmail.com>
2019-06-22 13:35:42 -0500

    Move old files to zip

--- DesignDocuments/updateDb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-file="/home/blake/Projects/questions/DesignDocuments/databaseDesign.sql"
-
-mysql --user="enquirio" --password --execute="USE enquirio; source $file; USE enquirio_test; source $file;"
