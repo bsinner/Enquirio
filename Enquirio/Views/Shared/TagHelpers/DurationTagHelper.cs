e08187191b311d70d2909ab5020c35c1c7eee427
bsinner <blakesinner31@gmail.com>
2019-06-28 01:19:34 -0500

    Fix date display

--- Enquirio/Views/Shared/TagHelpers/DurationTagHelper.cs
+++ Enquirio/Views/Shared/TagHelpers/DurationTagHelper.cs
@@ -11,32 +11,52 @@ namespace Enquirio.Views.Shared.TagHelpers {
 
         public DateTime Date { get; set; }
 
-        private readonly SortedDictionary<int, string> _timeSpans = new SortedDictionary<int, string>() {
-            { 3600, "minute"}
-            , { 216000, "hour"}
-            , { 5184000, "day"}
-            , { 20736000, "week"}
-            , { 82944000, "month"}
-        };
-
         public override void Process(TagHelperContext context, TagHelperOutput output) {
             output.Content.SetContent(GetString());
         }
 
         private string GetString() {
+            var times = new SortedDictionary<int, string>() {
+                { 3600, "minute"}
+                , { 216000, "hour"}
+                , { 5184000, "day"}
+                , { 20736000, "week"}
+                , { 82944000, "month"}
+            };
+
             var duration = (long)(DateTime.Now - Date).TotalSeconds;
+            var previous = 60;
 
             if (duration < 60) return "seconds ago";
             
-            foreach (var time in _timeSpans) {
+            foreach (var time in times) {
                 if (duration < time.Key) {
-                    var result = duration / time.Key;
+                    var result = duration / previous;
 
-                    return $"{result} {time.Key + (result == 1 ? "" : "s")} ago";
+                    return $"{result} {time.Value + (result == 1 ? "" : "s")} ago";
                 }
+
+                previous = time.Key;
             }
 
             return $"on {Date.ToShortDateString()}";
+        }
+
+        private string FindMessage(SortedDictionary<int, string> times, long duration) {
+//            if (duration < 60) return "seconds ago";
+//
+//            using (var enumerator = times.GetEnumerator()) {
+//
+//                while (enumerator.MoveNext()) {
+//                    if (duration < enumerator.Current.Key) {
+//                        enumerator.MoveNext();
+//                        var result = duration / enumerator.Current.Key;
+//                        return $"{result} {enumerator.Current.Value + (result == 1 ? "" : "s")} ago";
+//                    }
+//                }
+//            }
+//
+            return null;
         }
     }
 }
