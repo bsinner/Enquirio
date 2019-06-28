b49fc63a4626519f0d523c787889dd183948541a
bsinner <blakesinner31@gmail.com>
2019-06-28 01:55:27 -0500

    Add tag helper test, move loop to new method

---
+++ Enquirio.Tests/DurationTagHelperTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Enquirio.Views.Shared.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Xunit;
+
+namespace Enquirio.Tests {
+    public class DurationTagHelperTest {
+
+        [Theory]
+        [InlineData(-.01, "seconds ago")]
+        [InlineData(-.5, "30 minutes ago")]
+        [InlineData(-1, "1 hour ago")]
+        [InlineData(-25, "2 days ago")]
+        [InlineData(-(24 * 7 * 3), "3 weeks ago")]
+        public void TestDuration(double hours, string expected) {
+
+            // Arrange
+            var context = new TagHelperContext(
+                new TagHelperAttributeList()
+                , new Dictionary<object, object>()
+                ,  "unique-id"
+            );
+
+            var output = new TagHelperOutput("duration"
+                , new TagHelperAttributeList()
+                , (cache, encoder) => 
+                    Task.FromResult<TagHelperContent>(new DefaultTagHelperContent())
+            );
+
+            // Act
+            var helper = new DurationTagHelper { Date = DateTime.Now.AddHours(hours) };
+            helper.Process(context, output);
+
+            // Assert
+            Assert.Equal(expected, output.Content.GetContent());
+        }
+//
+//        public void TestDurationUpperBound() {
+//
+//        }
+    }
+}
--- Enquirio/Views/Shared/TagHelpers/DurationTagHelper.cs
+++ Enquirio/Views/Shared/TagHelpers/DurationTagHelper.cs
@@ -17,45 +17,33 @@ namespace Enquirio.Views.Shared.TagHelpers {
 
         private string GetString() {
             var times = new SortedDictionary<int, string>() {
-                { 3600, "minute"}
-                , { 216000, "hour"}
-                , { 5184000, "day"}
-                , { 20736000, "week"}
-                , { 82944000, "month"}
+                { 60, "minute"}
+                , { 3600, "hour"}
+                , { 86400, "day"}
+                , { 604800, "week"}
+                , { 2419200, "month"}
             };
 
             var duration = (long)(DateTime.Now - Date).TotalSeconds;
-            var previous = 60;
 
             if (duration < 60) return "seconds ago";
-            
-            foreach (var time in times) {
-                if (duration < time.Key) {
-                    var result = duration / previous;
 
-                    return $"{result} {time.Value + (result == 1 ? "" : "s")} ago";
-                }
-
-                previous = time.Key;
-            }
-
-            return $"on {Date.ToShortDateString()}";
+            return LoopTimes(times, duration) ?? $"on {Date.ToShortDateString()}";
         }
 
-        private string FindMessage(SortedDictionary<int, string> times, long duration) {
-//            if (duration < 60) return "seconds ago";
-//
-//            using (var enumerator = times.GetEnumerator()) {
-//
-//                while (enumerator.MoveNext()) {
-//                    if (duration < enumerator.Current.Key) {
-//                        enumerator.MoveNext();
-//                        var result = duration / enumerator.Current.Key;
-//                        return $"{result} {enumerator.Current.Value + (result == 1 ? "" : "s")} ago";
-//                    }
-//                }
-//            }
-//
+        private string LoopTimes(SortedDictionary<int, string> times, long duration) {
+            var previous = new KeyValuePair<int, string>(60, "seconds");
+
+            foreach (var time in times) {
+                if (duration < time.Key) {
+                    var result = duration / previous.Key;
+
+                    return $"{result} {previous.Value + (result == 1 ? "" : "s")} ago";
+                }
+
+                previous = time;
+            }
+
             return null;
         }
     }
