dd8405378708d817ce4bc3dec573ba302aab5807
bsinner <blakesinner31@gmail.com>
2019-07-01 15:52:06 -0500

    Update test data

--- Enquirio.Tests/TestData/QuestionData.cs
+++ Enquirio.Tests/TestData/QuestionData.cs
@@ -6,10 +6,13 @@ using Enquirio.Models;
 namespace Enquirio.Tests.TestData {
     class QuestionData {
         public static Question TestQuestion
-            = new Question { Id = 99, Title = "Q2", Contents = "...", Created = DateTime.Now
+            = new Question { Id = 0, Title = "Q1", Contents = "...", Created = DateTime.Now
                 , Answers = new List<Answer> {
                     new Answer { Id = 1, Title = "A1", Contents = "...", Created = DateTime.Now }
                     , new Answer { Id = 2, Title = "A2", Contents = "...", Created = DateTime.Now }}
             };
+
+        public static Answer TestAnswer
+            = new Answer {Id = 0, Title = "A1", Contents = "...", Created = DateTime.Now, QuestionId = 1};
     }
 }
