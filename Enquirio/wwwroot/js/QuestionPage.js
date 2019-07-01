1c4ebfcd2fe5cec8577dffacdbbac23a230ac080
bsinner <blakesinner31@gmail.com>
2019-07-01 18:29:09 -0500

    Add show hide to post new answer form

---
+++ Enquirio/wwwroot/js/QuestionPage.js
@@ -0,0 +1,20 @@
+﻿window.onload = () => {
+
+    const _newAnswerForm = document.getElementById("answerForm");
+    const _showAnswerFormBtn = document.getElementById("showAnsFormBtn");
+
+    _showAnswerFormBtn.onclick = () => {
+        toggleElements(_newAnswerForm, _showAnswerFormBtn);
+    };
+
+    document.getElementById("hideAnsFormBtn").onclick = () => {
+        toggleElements(_showAnswerFormBtn, _newAnswerForm);
+    };
+
+    // Show one element, hide the other
+    function toggleElements(toShow, toHide) {
+        toShow.removeAttribute("style");
+        toHide.setAttribute("style", "display: none;");
+    }
+
+};
