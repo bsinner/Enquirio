6895778652388914e0885a1e0068c5b3de75aef0
bsinner <blakesinner31@gmail.com>
2019-07-01 18:47:15 -0500

    Add edit and delete buttons

--- Enquirio/wwwroot/js/QuestionPage.js
+++ Enquirio/wwwroot/js/QuestionPage.js
@@ -1,14 +1,18 @@
 ﻿window.onload = () => {
 
+    // Elements
     const _newAnswerForm = document.getElementById("answerForm");
+    const _questionBtns = document.getElementById("questionBtns");
+
+    // Buttons
     const _showAnswerFormBtn = document.getElementById("showAnsFormBtn");
 
     _showAnswerFormBtn.onclick = () => {
-        toggleElements(_newAnswerForm, _showAnswerFormBtn);
+        toggleElements(_newAnswerForm, _questionBtns);
     };
 
     document.getElementById("hideAnsFormBtn").onclick = () => {
-        toggleElements(_showAnswerFormBtn, _newAnswerForm);
+        toggleElements(_questionBtns, _newAnswerForm);
     };
 
     // Show one element, hide the other
