899bd6ad9e4aa994230195b11fc44331ca0f6cc9
bsinner <blakesinner31@gmail.com>
2019-07-04 18:56:43 -0500

    Add create answer test and method

--- Enquirio.Tests/Tests/Controllers/QuestionApiTest.cs
+++ Enquirio.Tests/Tests/Controllers/QuestionApiTest.cs
@@ -1,20 +1,51 @@
-﻿namespace Enquirio.Tests.Tests.Controllers {
-    class QuestionApiTest {
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Enquirio.Controllers;
+using Enquirio.Data;
+using Enquirio.Tests.TestData;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Enquirio.Tests.Tests.Controllers {
+    public class QuestionApiTest {
         
+        [Fact]
         public void CreateAnswerTest() {
+            // Arrange
+            var mockRepo = new Mock<IRepositoryEnq>();
+            var answer = QuestionData.TestAnswer;
 
+            mockRepo.Setup(repo => repo.Create(answer));
+            mockRepo.Setup(repo => repo.SaveAsync()).Callback(() => answer.Id = 1);
+
+            var controller = new QuestionApiController(mockRepo.Object);
+
+            // Act
+            Task<string> id = controller.CreateAnswer(answer);
+
+            // Assert
+            Assert.Equal("1", id.Result);
+
+            Assert.NotEmpty(typeof(QuestionApiController)
+                .GetMethod(nameof(QuestionApiController.CreateAnswer))
+                .GetCustomAttributes(typeof(HttpPostAttribute), false));
+
+            mockRepo.Verify(repo => repo.Create(answer));
+            mockRepo.Verify(repo => repo.SaveAsync());
         }
 
+        [Fact]
         public void EditAnswerTest() {
-
         }
 
+        [Fact]
         public void DeleteAnswerTest() {
-
         }
 
+        [Fact]
         public void EditQuestionTest() {
-
         }
 
     }
--- Enquirio/Controllers/QuestionApiController.cs
+++ Enquirio/Controllers/QuestionApiController.cs
@@ -1,4 +1,25 @@
-﻿namespace Enquirio.Controllers {
-    public class QuestionApiController {
+﻿using System.Threading.Tasks;
+using Enquirio.Data;
+using Enquirio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Enquirio.Controllers {
+
+    [Route("api/[controller]")]
+    public class QuestionApiController : Controller {
+
+        private readonly IRepositoryEnq _repo;
+
+        public QuestionApiController(IRepositoryEnq repo) => _repo = repo;
+
+        [HttpPost]
+        public async Task<string> CreateAnswer([FromBody] Answer answer) {
+            _repo.Create(answer);
+
+            await _repo.SaveAsync();
+
+            return answer.Id.ToString();
+        }
+
     }
 }
