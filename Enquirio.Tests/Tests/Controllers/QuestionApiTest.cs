d8f34f83d123c495d6d422c890a7de8ea13393e8
bsinner <blakesinner31@gmail.com>
2019-07-05 21:58:13 -0500

    Add verb test, update CreateAnswerTest
    Added a test to test HTTP verb attributes of controller methods, updated
    CreateAnswerTest to use the new return type of the method it tests, and
    to assert that ExistsAsync was called.

--- Enquirio.Tests/Tests/Controllers/QuestionApiTest.cs
+++ Enquirio.Tests/Tests/Controllers/QuestionApiTest.cs
@@ -20,48 +20,63 @@ namespace Enquirio.Tests.Tests.Controllers {
                 // Arrange
                 mockRepo.Setup(repo => repo.Create(answer));
                 mockRepo.Setup(repo => repo.SaveAsync()).Callback(() => answer.Id = 50);
+                mockRepo.Setup(repo => repo.ExistsAsync<Question>(e => e.Id == 1))
+                    .ReturnsAsync(true);
 
                 // Act
-                Task<string> id = controller.CreateAnswer((Answer)answer, 99);
+                OkObjectResult result = controller
+                    .CreateAnswer((Answer)answer, 1)
+                    .Result as OkObjectResult;
 
                 // Assert
-                Assert.Equal("50", id.Result);
-                Assert.True(HasAttribute(nameof(QuestionApiController.CreateAnswer)
-                    , typeof(HttpPostAttribute)));
+                Assert.Equal("50", result.Value);
 
-                mockRepo.Verify(repo => repo.Create(answer));
-                mockRepo.Verify(repo => repo.SaveAsync());
+                mockRepo.Verify(repo => repo.Create(answer), Times.Once);
+                mockRepo.Verify(repo => repo.SaveAsync(), Times.Once);
+                mockRepo.Verify(repo => repo.ExistsAsync<Question>(e => e.Id == 1), Times.Once);
             });
         }
 
         [Fact]
         public void CreateAnswerErrorTest() {
-            RunTest((answer, err, mockRepo, controller) => {
+            RunTest((answer, errAnswer, mockRepo, controller) => {
+                
                 // Arrange
+                mockRepo.Setup(repo => repo.ExistsAsync<Question>(e => e.Id == 999))
+                    .ReturnsAsync(false);
+
                 // Act
+                NotFoundResult result404 = controller
+                    .CreateAnswer((Answer)answer, 999)
+                    .Result as NotFoundResult;
+
                 // Assert
             });
         }
 
         [Fact]
         public void EditAnswerTest() {
-//            RunTest((answer, err, mockRepo, controller) => {
-//
-//                // Arrange
-//                mockRepo.Setup(repo => repo.Update(answer));
-//                mockRepo.Setup(repo => repo.SaveAsync());
-//
-//                // Act
-//                Task<StatusCodeResult> result = controller.EditAnswer(answer);
-//
-//                // Assert
-//                Assert.Equal(201, result.Result.StatusCode);
-//                Assert.True(HasAttribute(nameof(QuestionApiController.EditAnswer)
-//                    , typeof(HttpPutAttribute)));
-//                
-//                mockRepo.Verify(repo => repo.Update(answer), Times.Once);
-//                mockRepo.Verify(repo => repo.SaveAsync(), Times.Once);
-//            });
+            RunTest((answer, err, mockRepo, controller) => {
+
+                // Arrange
+                mockRepo.Setup(repo => repo.Update(answer));
+                mockRepo.Setup(repo => repo.SaveAsync());
+
+                // Act
+                Task<StatusCodeResult> result = controller.EditAnswer(answer);
+
+                // Assert
+                Assert.Equal(201, result.Result.StatusCode);
+                Assert.True(HasAttribute(nameof(QuestionApiController.EditAnswer)
+                    , typeof(HttpPutAttribute)));
+                
+                mockRepo.Verify(repo => repo.Update(answer), Times.Once);
+                mockRepo.Verify(repo => repo.SaveAsync(), Times.Once);
+            });
+        }
+
+        [Fact]
+        public void EditAnswerErrorTest() {
         }
 
         [Fact]
@@ -69,13 +84,36 @@ namespace Enquirio.Tests.Tests.Controllers {
         }
 
         [Fact]
+        public void DeleteAnswerErrorTest() {
+        }
+
+        [Fact]
         public void EditQuestionTest() {
+        }
+
+        [Fact]
+        public void EditQuestionErrorTest() {
+        }
+
+        [Fact]
+        public void HttpVerbTests() {
+            Assert.True(HasAttribute(nameof(QuestionApiController.CreateAnswer)
+                , typeof(HttpPostAttribute)));
+
+            Assert.True(HasAttribute(nameof(QuestionApiController.DeleteAnswer)
+                , typeof(HttpDeleteAttribute)));
+
+            Assert.True(HasAttribute(nameof(QuestionApiController.EditAnswer)
+                , typeof(HttpPutAttribute)));
+
+            Assert.True(HasAttribute(nameof(QuestionApiController.EditQuestion)
+                , typeof(HttpPutAttribute)));
         }
 
         // Run test, pass valid entity, invalid entity, repository, and controller
         private void RunTest(Action<IPost, IPost, Mock<IRepositoryEnq>, QuestionApiController> test, bool questionTest = false) {
-            IPost entity = null;
-            IPost invalidEntity = null;
+            IPost entity;
+            IPost invalidEntity;
 
             if (!questionTest) {
                 entity = QuestionData.TestAnswer;
