1af1678f39cbf9e8be3ee6ca7e671d3c5425b24b
bsinner <blakesinner31@gmail.com>
2019-07-06 21:57:30 -0500

    Add hide functions, update elem toggle, change ids

--- Enquirio/wwwroot/js/QuestionPage.js
+++ Enquirio/wwwroot/js/QuestionPage.js
@@ -4,22 +4,19 @@
     const _questionId = document.getElementById("qid").innerText;
     const _baseUrl = document.getElementById("basePath").innerText.trim();
 
+    // Buttons
+    const _hideEditQ = document.getElementById("hideQEdit");
+    const _hideNewA = document.getElementById("hideAnsFormBtn");
+    // const _hideEditA = document.getElementById("");
+
     // Elements
     const _newAnswerForm = document.getElementById("answerForm");
     const _questionBtns = document.getElementById("questionBtns");
-
-    // Buttons
-    const _showAnswerFormBtn = document.getElementById("showAnsFormBtn");
-    const _questionDeleteBtn = document.getElementById("deleteQuestionBtn");
-    const _submitAnswer = document.getElementById("submitAnswer");
-
-    // Show create answer form
-    _showAnswerFormBtn.onclick = () => {
-        toggleElements(_newAnswerForm, _questionBtns);
-    };
+    const _editQForm = document.getElementById("qEditForm");
+    const _qTextBtns = [document.getElementById("questionText"), _questionBtns];
 
     // Submit answer
-    _submitAnswer.onclick = () => {
+    document.getElementById("submitAnswer").onclick = () => {
         const newTitle = document.getElementById("newAnsTitle").value;
         const newContent = document.getElementById("newAnsContent").value;
 
@@ -35,14 +32,45 @@
         });
     };
 
-    document.getElementById("hideAnsFormBtn").onclick = () => {
+    // Hide edit question form
+    _hideEditQ.onclick = () => {
+        toggleElements(_qTextBtns, _editQForm);
+    }
+
+    // Show edit question form
+    document.getElementById("showQEdit").onclick = () => {
+        toggleElements(_editQForm, _qTextBtns);
+    }
+
+    // Hide create answer form
+    _hideNewA.onclick = () => {
         toggleElements(_questionBtns, _newAnswerForm);
     };
 
+    // Show create answer form
+    document.getElementById("showAnsFormBtn").onclick = () => {
+        toggleElements(_newAnswerForm, _questionBtns);
+    };
+
     // Show one element, hide the other
+    // params can be one element or an array of elements
     function toggleElements(toShow, toHide) {
-        toShow.removeAttribute("style");
-        toHide.setAttribute("style", "display: none;");
+        const show = e => e.removeAttribute("style");
+        const hide = e => e.setAttribute("style", "display: none;");
+
+        if (toShow instanceof Array) {
+            toShow.forEach(e => { show(e); });
+        } else {
+            show(toShow);
+        }
+
+        if (toHide instanceof Array) {
+            toHide.forEach(e => { hide(e); });
+        } else {
+            hide(toHide);
+        }
     }
 
+    // Clear form errors
+    function clearErrState() {}
 };
