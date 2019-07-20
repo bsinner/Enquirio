11a244c87f9b6eaf424ea45455acb5796c1eb361
bsinner <blakesinner31@gmail.com>
2019-07-20 17:00:31 -0500

    Change utils to module

--- Enquirio/wwwroot/js/QuestionPage/QuestionPageUtil.js
+++ Enquirio/wwwroot/js/QuestionPage/QuestionPageUtil.js
@@ -1,17 +1,21 @@
-﻿// Function for displaying errors and showing hidden elements
-function QuestionPageUtils() {
+﻿// Class for showing errors and showing elements
+export default class QuestionPageUtil {
+
+    constructor() {
+        this.helper = new Helper();
+    }
 
     // Show one element, hide the other.
     // Params can be one element or an array of elements
-    this.toggleElements = (toShow, toHide) => {
-        loopOrSingle(toShow, show);
-        loopOrSingle(toHide, hide);
+    toggleElements(toShow, toHide) {
+        this.helper.loopOrSingle(toShow, show);
+        this.helper.loopOrSingle(toHide, hide);
     }
 
     // Highlight empty inputs and show error message, return true if errors
     // where found. Param must be an array of objects in the following format:
     // [ { elem : inputElem, msg : "optional error message" } ]
-    this.errIfEmpty = inputs => {
+    errIfEmpty(inputs) {
         let results = false;
 
         inputs.forEach(obj => {
@@ -24,9 +28,9 @@ function QuestionPageUtils() {
                 const small = input.parentElement.querySelector("small");
 
                 if ("msg" in obj) {
-                    this.showError(input, small, msg);
+                    showError(input, small, msg);
                 } else {
-                    this.showError(input, small);
+                    showError(input, small);
                 }
             }
         });
@@ -36,41 +40,43 @@ function QuestionPageUtils() {
 
     // Highlight input, show error message. Error message can be
     // changed through optional msg parameter
-    this.showError = (input, small, msg = null) => {
+    showError(input, small, msg = null) {
         if (msg) {
             small.innerText = msg;
         }
 
-        addInputErr(input);
-        show(small);
+        this.helper.addInputErr(input);
+        this.helper.show(small);
 
         input.oninput = () => {
-            removeInputErr(input);
-            hide(small);
+            this.helper.removeInputErr(input);
+            this.helper.hide(small);
         }
     }
 
     // Hide all of a divs small elements containing error class text-danger, 
     // un-highlight form fields with error class is-invalid
-    this.clearErrors = div => {
+    clearErrors(div) {
         div.querySelectorAll("small.text-danger").forEach(elem => {
-            hide(elem);
+            this.helper.hide(elem);
         });
 
         div.querySelectorAll(".is-invalid").forEach(elem => {
-            removeInputErr(elem);
+            this.helper.removeInputErr(elem);
         });
     }
+}
 
-    function show(e) { e.removeAttribute("style"); }
+class Helper {
+    show(e) { e.removeAttribute("style"); }
 
-    function hide(e) { e.setAttribute("style", "display: none;"); }
+    hide(e) { e.setAttribute("style", "display: none;"); }
 
-    function addInputErr(i) { i.classList.add("is-invalid"); }
+    addInputErr(i) { i.classList.add("is-invalid"); }
 
-    function removeInputErr(i) { i.classList.remove("is-invalid"); }
+    removeInputErr(i) { i.classList.remove("is-invalid"); }
 
-    function loopOrSingle(item, callback) {
+    loopOrSingle(item, callback) {
         if (item instanceof Array) {
             item.forEach(i => {
                 callback(i);
