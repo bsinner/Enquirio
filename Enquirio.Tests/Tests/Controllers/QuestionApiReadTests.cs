ad5721a4a065264a939848fe24480c654ae8a3d8
bsinner <blakesinner31@gmail.com>
2019-08-19 21:57:57 -0500

    Add get page one of questions test

--- Enquirio.Tests/Tests/Controllers/QuestionApiReadTests.cs
+++ Enquirio.Tests/Tests/Controllers/QuestionApiReadTests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Enquirio.Controllers;
 using Enquirio.Data;
 using Enquirio.Models;
@@ -14,13 +15,31 @@ using Xunit;
 namespace Enquirio.Tests.Tests.Controllers {
     public class QuestionApiReadTests : ApiTestUtil {
 
+        private const int PageLength = QuestionApiController.PageLength;
+
         [Fact]
-        public void TestGetQuestionsPageOne() {
+        public async Task TestGetQuestionsPageOne() {
+            // Arrange
             var mockRepo = new Mock<IRepositoryEnq>();
             mockRepo.Setup(repo => repo.GetAllAsync<Question>
-                (null, null, true, 10, null, null))
-                .ReturnsAsync(QuestionData.TestQuestions().ToList().GetRange(0, 10));
+                (null, null, true, PageLength, null, null))
+                .ReturnsAsync
+                (QuestionData.TestQuestions().ToList().GetRange(0, 10));
 
+            var controller = new QuestionApiController(mockRepo.Object);
+
+            // Act 
+            var result = await controller.GetQuestions();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            Assert.IsType<List<Question>>(result.Value);
+            Assert.Equal(PageLength
+                , (result.Value as List<Question>).Count);
+
+            mockRepo.Verify(repo => repo.GetAllAsync<Question>
+                (null, null, true, PageLength, null, null), Times.Once);
+            mockRepo.VerifyNoOtherCalls();
         }
 
         [Fact]
