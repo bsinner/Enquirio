bd4dd66d623594832ecf62017c186284cacacc8f
bsinner <blakesinner31@gmail.com>
2019-08-23 21:56:47 -0500

    Add list of questions to home page

---
+++ Enquirio/frontend/src/components/home/QuestionList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-sm-9">        
+            <div v-for="(q, index) in questions"
+                v-bind:key="index">
+
+                    <h6>{{ q.title }}</h6>
+
+                    <p class="mb-4">
+                        {{ q.contents }}
+                        <router-link :to="`/question/${ q.id }`">
+                            (More...)
+                        </router-link>            
+                        <i class="text-secondary">
+                            &nbsp;&nbsp;&nbsp;Username - {{ q.created }}
+                        </i>
+                    </p>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+    computed: { ...mapState("home", { 
+        questions: s => s.questions, 
+        pageNumber: s => s.pageNumber
+    }) },
+    methods: { ...mapActions("home", [ "getPosts" ]) },
+    created() {
+        this.getPosts(this.pageNumber);
+    }
+}
+</script>
--- Enquirio/frontend/src/views/Home.vue
+++ Enquirio/frontend/src/views/Home.vue
@@ -2,14 +2,27 @@
     <div>      
         <br><br>
         <search-bar></search-bar>
-        <!-- foreach loop that displays question components using data from created() -->
+
+        <br>
+        <div class="row justify-content-center">
+            <div class="col-sm-11">
+                
+                <h5 class="text-center">Recent Questions Within X miles of XXXXX</h5>
+                <hr>
+
+                <question-list></question-list>
+                
+                <!-- <page-controls></page-controls> -->            
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    import SearchBar from "../components/home/SearchBar";
+    import SearchBar from "../components/home/SearchBar";    
+    import QuestionList from "../components/home/QuestionList";
 
     export default {
-        components: { SearchBar }
+        components: { SearchBar, QuestionList },        
     }
 </script>
