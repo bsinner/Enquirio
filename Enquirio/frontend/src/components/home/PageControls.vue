cbd74722d0bc27d4f6c22285f38e95bd17d1333d
bsinner <blakesinner31@gmail.com>
2019-08-25 02:06:46 -0500

    Add shift page buttons forward on going past 10

--- Enquirio/frontend/src/components/home/PageControls.vue
+++ Enquirio/frontend/src/components/home/PageControls.vue
@@ -11,11 +11,11 @@
                 <i class="fas fa-chevron-left"></i>
             </label>
 
-            <label v-for="(p, i) in pageArray"
+            <label v-for="(p, i) in btnArray"
                     :key="i" class="btn btn-secondary"
                     :class="{ active : p === page }">
                 <input type="radio" autocomplete="off"
-                        @click="getQuestions(p)">{{ p }}
+                        @click="toPage(p)">{{ p }}
             </label>
 
             <label v-if="pages >= 10 && page < pages" 
@@ -40,29 +40,78 @@
  * - If pages is greater than 9 and less than 16 show 10 buttons at a time
  *   and include arrow buttons to move left and right
  * - If pages is greater than 15, left and right arrows move one page forward
- *   and shift the display 10 numbers forward, an icon for the first and/or 
+ *   and shift the display multiple numbers forward, an icon for the first and/or 
  *   last page is shown, and an input for entering a page to view is shown
  */
 import { mapState, mapActions } from "vuex";
 
+const MAX_BTNS = 10;
+const SHIFT_LG = 9;
+const SHIFT_SM = 2;
+const START_LG = 16;
+
 export default {    
+    data() {
+        return { btnArray: [1] };
+    },
     computed: { 
         ...mapState("home", { 
             pages: s => s.pages,
             page: s => s.pageNumber
         }),
-        pageArray() {                             
-            return [...Array(this.pages + 1).keys()].splice(1);     
-        },
     },
     methods: { 
         ...mapActions("home", [ "getPages", "getQuestions" ]),
-        toPage(p) {
-            this.getQuestions(p);
+        // Get the previous or next page of questions
+        toPage(p) {    
+
+            // if page is valid continue
+            if (p <= this.pages) {
+                const lng = this.btnArray.length - 1;
+                
+                // if requested page number is not one of the numbered 
+                // buttons on screen update the list of buttons
+                if (p >= this.btnArray[lng]) {
+                
+                    if (this.pages < START_LG) { 
+                        this.shiftPageButtons(SHIFT_SM, p);
+                    } else {
+                        this.shiftPageButtons(SHIFT_LG, p);
+                    }
+                }
+                
+                this.getQuestions(p);
+            }
+        },
+        // Update the page buttons if the last or first button was clicked
+        shiftPageButtons(dist, requestedPage) {
+        
+            // Go forward
+            if (dist > 0) {
+                const remainingPages = this.pages - requestedPage;
+                const lng = () => this.btnArray.length;
+
+                if (dist > remainingPages) {                    
+                    dist = remainingPages;
+                }
+                
+                for (let i = 0; i < dist; i++) {                
+                    const last = this.btnArray[lng() - 1];            
+                    this.btnArray.push(last + 1);                    
+                }
+
+                this.btnArray = this.btnArray.splice(lng() - MAX_BTNS, lng());
+                
+            // Go backwards                
+            } else {
+                
+            }
         }
     },
-    created() {
+    created() {        
         this.getPages();
+        this.btnArray = [...Array(this.pages + 1).keys()]
+                .splice(1, MAX_BTNS);
     }
 }
 
