6a77fb8838aa56156de735ce8a36308c711bd647
bsinner <blakesinner31@gmail.com>
2019-08-25 03:55:46 -0500

    Split up methods

--- Enquirio/frontend/src/components/home/PageControls.vue
+++ Enquirio/frontend/src/components/home/PageControls.vue
@@ -32,17 +32,6 @@
 </template>
 
 <script> 
-/*
- * Possible pagination displays
- * - If there is only one page of results don't display buttons
- * - If pages is greater than 1 and less than 10 display a button for
- *   each page number
- * - If pages is greater than 9 and less than 16 show 10 buttons at a time
- *   and include arrow buttons to move left and right
- * - If pages is greater than 15, left and right arrows move one page forward
- *   and shift the display multiple numbers forward, an icon for the first and/or 
- *   last page is shown, and an input for entering a page to view is shown
- */
 import { mapState, mapActions } from "vuex";
 
 const MAX_BTNS = 10;
@@ -64,78 +53,85 @@ export default {
     },
     methods: { 
         ...mapActions("home", [ "getPages", "getQuestions" ]),
-        // Get a page of questions
-        toPage(p) {    
-
-            // if page isn't greater than total number of pages continue
-            if (p <= this.pages) {
-                const lng = this.btnArray.length - 1;
-                
-                // if requested page number is not on the screen update the controls
-                if (p >= this.btnArray[lng]) {
-                    
-                    // Update to show going forward
-                    if (this.pages < START_LG) { 
-                        this.shiftPageButtons(SHIFT_SM, p);
-                    } else {
-                        this.shiftPageButtons(SHIFT_LG, p);
-                    }
-                } else if (p <= this.btnArray[0] && p > 1) {
-                    
-                    // Update to show going back
-                    if (this.pages < START_LG) {
-                        this.shiftPageButtons(-SHIFT_SM, p);
-                    } else {
-                        this.shiftPageButtons(-SHIFT_LG, p);
-                    }
-                }
-                                
+        
+        // Load page of questions
+        toPage(p) {
+            if (p <= this.pages) {                
+                this.updateControls(p);
                 this.getQuestions(p);
             }
         },
         
-        // Show more page buttons if the first or last button was clicked
+        // Load more page buttons if the first or last button was clicked
+        updateControls(p) {
+            const lng = this.btnArray.length - 1;
+
+            // Update buttons if last was clicked
+            if (p >= this.btnArray[lng]) {
+                
+                if (this.pages < START_LG) { 
+                    this.shiftPageButtons(SHIFT_SM, p); 
+                } else { 
+                    this.shiftPageButtons(SHIFT_LG, p); 
+                }
+            // Update buttons if first was clicked
+            } else if (p <= this.btnArray[0] && p > 1) {
+
+                if (this.pages < START_LG) {
+                    this.shiftPageButtons(-SHIFT_SM, p);                
+                } else {
+                    this.shiftPageButtons(-SHIFT_LG, p);
+                }
+            }
+        },
+
+        // Show more page buttons, hide old ones
         shiftPageButtons(dist, requestedPage) {                        
             // Boolean to represent going backwards or forwards
             const isNeg = dist < 0;
             dist = Math.abs(dist);
             
+            // Shorthand for .length
+            const lng = () => this.btnArray.length;
+
             // Pages left between start/end of btnArray and total pages
             const remainingPages = isNeg 
                     ? this.btnArray[0] - 1
                     : this.pages - requestedPage;
-
-            // Shorthand for .length
-            const lng = () => this.btnArray.length;
-
-            // if trying to go more pages than there are left 
-            // change distance to amount of remaing pages
+        
+            // Prevent creating more buttons than there are pages
             if (dist > remainingPages) {                    
                 dist = remainingPages;
             }
             
-            // Append new buttons
-            for (let i = 0; i < dist; i++) {
-                // Last/first element in btnArray
-                const borderElement = isNeg 
+            this.appendPageButtons(isNeg, dist, lng);
+            this.removeOldButtons(isNeg, lng);
+        }, 
+        
+        // Append more buttons to start or end of btnArray
+        appendPageButtons(isNegative, distance, lng) {
+            for (let i = 0; i < distance; i++) {
+                const borderElement = isNegative 
                         ? this.btnArray[0]
                         : this.btnArray[lng() - 1];
                 
-                if (isNeg) {
+                if (isNegative) {
                     this.btnArray.splice(0, 0, borderElement - 1);
                 } else {
                     this.btnArray.push(borderElement + 1);
-                }                
-            }
-            
-            // Trim old buttons
-            this.btnArray = isNeg 
+                }
+            }                    
+        }, 
+        
+        // Remove extra buttons from start or end of btnArray
+        removeOldButtons(isNegative, lng) {
+            this.btnArray = isNegative
                     ? this.btnArray.splice(0, MAX_BTNS)
-                    : this.btnArray.splice(lng() - MAX_BTNS, lng());                                                
+                    : this.btnArray.splice(lng() - MAX_BTNS, lng());
         }
     },
     created() {        
-        this.getPages();
+        this.getPages();    
         this.btnArray = [...Array(this.pages + 1).keys()]
                 .splice(1, MAX_BTNS);
     }
