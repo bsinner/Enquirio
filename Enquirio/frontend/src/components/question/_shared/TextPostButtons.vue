07c2d4b1436f0afb31d578e2b381cbf36d7f82ca
bsinner <blakesinner31@gmail.com>
2019-08-27 03:53:50 -0500

    Add form component, update buttons component

--- Enquirio/frontend/src/components/question/_shared/TextPostButtons.vue
+++ Enquirio/frontend/src/components/question/_shared/TextPostButtons.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <slot></slot>
-        <button class="btn btn-outline-info mr-2 float-right">Edit</button>
-        <button class="btn btn-outline-info mr-2 float-right">Delete</button>
+        <button class="btn btn-outline-info mr-2 float-right"
+                :click="$emit('showEdit')">Edit
+        </button>
+        <button class="btn btn-outline-info mr-2 float-right"
+                :click="$emit('deleteItem')">Delete
+        </button>
     </div>
 </template>
---
+++ Enquirio/frontend/src/components/question/_shared/TextPostForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        
+        <br>
+        <div>
+            <input type="text" class="form-control">
+            <small class="text-danger" v-if="titleError">
+                Title must not be blank
+            </small>
+        </div>
+
+        <br>
+        <div>
+            <textarea rows="5" class="form-control"></textarea>
+            <small class="text-danger" v-if="contentsError">
+                Contents must not be blank
+            </small>
+        </div>
+
+        <br>
+        <button class="btn btn-outline-info float-right" 
+                @click="$emit('hideForm')">Cancel
+        </button>
+        <button class="btn btn-outline-dark float-right mr-2"
+                @click="$emit('submitForm')">Submit
+        </button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() { 
+        return {
+            titleError: false,
+            contentsError: false
+        }
+    }
+}
+</script>
