2f46cec1abfac083d9804b4d90e05ca130b75e6f
bsinner <blakesinner31@gmail.com>
2019-08-29 20:00:57 -0500

    Complete generic question/answer form component

--- Enquirio/frontend/src/components/question/_shared/TextPostForm.vue
+++ Enquirio/frontend/src/components/question/_shared/TextPostForm.vue
@@ -3,7 +3,9 @@
         
         <br>
         <div>
-            <input type="text" class="form-control">
+            <input v-model="localTitle"
+                    @input="titleError = false"              
+                    type="text" class="form-control">
             <small class="text-danger" v-if="titleError">
                 Title must not be blank
             </small>
@@ -11,7 +13,9 @@
 
         <br>
         <div>
-            <textarea rows="5" class="form-control"></textarea>
+            <textarea v-model="localContents"
+                    @input="contentsError = false"                   
+                    rows="5" class="form-control"></textarea>
             <small class="text-danger" v-if="contentsError">
                 Contents must not be blank
             </small>
@@ -22,7 +26,7 @@
                 @click="$emit('hideForm')">Cancel
         </button>
         <button class="btn btn-outline-dark float-right mr-2"
-                @click="$emit('submitForm')">Submit
+                @click="submitForm">Submit
         </button>
 
     </div>
@@ -30,11 +34,41 @@
 
 <script>
 export default {
+    props: [ "title", "contents" ],
     data() { 
         return {
             titleError: false,
-            contentsError: false
+            contentsError: false,
+            localTitle: "",
+            localContents: ""
         }
+    }, methods: {
+        submitForm($event) {
+            // If neither feild is empty emit title and contents          
+            if (this.localTitle && this.localContents) {                
+                this.$emit("submitForm", 
+                        this.localTitle , this.localContents                             
+                );
+            // Else highlight the empty fields
+            } else {
+                this.titleError = !this.localTitle 
+                        ? true : false;
+                this.contentsError = !this.localContents 
+                        ? true : false;
+            }
+        },
+        updateContents($event) {        
+            this.localContents = $event.target.value;            
+        },
+        updateTitle($event) {
+            this.localTitle = $event.target.value;
+        }
+    // Form title and contents are stored localy instead of bound to 
+    // the parent because the parent should update when the form 
+    // submits instead of on input
+    }, created() {
+        this.localTitle = this.title;
+        this.localContents = this.contents;
     }
 }
 </script>
