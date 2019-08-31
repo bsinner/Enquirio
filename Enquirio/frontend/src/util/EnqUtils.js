d621a0ba9ab8fb8bb0fb80192192598b97a7b887
bsinner <blakesinner31@gmail.com>
2019-08-30 23:02:28 -0500

    Add getWithoutProperties

--- Enquirio/frontend/src/util/EnqUtils.js
+++ Enquirio/frontend/src/util/EnqUtils.js
@@ -1,14 +1,13 @@
 export { ObjUtil };
 
-/*
- * Convert a nested array of objects in an object into a map, for example:
- * { foo: [ {id:4, x:"a4"}, {id:99, x:"a99"} ] }
- * becomes
- * { foo: { 4: {id:4, x:"a4"}, 99: {id:99, x:"a99"} } }
- * Also map can be converted back with mapToArray
- */
 const ObjUtil = (function() {
 
+    /*
+     * Convert an array of objects within an object into a map
+     * { foo: [ {id:4, x:"a4"}, {id:99, x:"a99"} ]
+     * becomes
+     * { foo: { 4: {id:4, x:"a4"}, 99: {id:99, x:"a99"} } 
+     */
     function arrayToMap(obj, array, key) {
         const temp = {};
 
@@ -19,18 +18,48 @@ const ObjUtil = (function() {
         obj[array] = temp;
     }
 
+    /* 
+     * Convert a map within an object created by arrayToMap 
+     * back into an array
+     */
     function mapToArray(obj, map) {
         const temp = [];
 
-        for (let [key, val] of Object.entries(obj[map])) {
-            temp.push(val);
-        }
+        _loopObject(obj[map], (k, value) => {
+            temp.push(value);
+        });
 
         obj[map] = temp;
     }
 
+    /*
+     * Get a shallow copy of an object and skip given properties,
+     * toSkip can be an array or a single string
+     */
+    function getWithoutProperties(obj, toSkip) {
+        const temp = {};
+        let skipKey = toSkip instanceof Array ?
+            k => toSkip.includes(k) :
+            k => toSkip === k;
+
+        _loopObject(obj, (key, value) => {
+            if (!skipKey(key)) {
+                temp[key] = value;
+            }
+        });
+
+        return temp;
+    }
+
+    function _loopObject(obj, callback) {
+        for (let [k, v] of Object.entries(obj)) {
+            callback(k, v);
+        }
+    }
+
     return {
         arrayToMap: arrayToMap,
-        mapToArray: mapToArray
+        mapToArray: mapToArray,
+        getWithoutProperties: getWithoutProperties
     };
 })();
